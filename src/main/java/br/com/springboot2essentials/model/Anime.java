b9c996423c7ca4d9eb1cfb586fe8848e401f4c0d
rauldangio <jojofagqwrt@gmail.com>
2023-04-11 21:27:04 -0300

    terminar aula do JPA

--- src/main/java/br/com/springboot2essentials/model/Anime.java
+++ src/main/java/br/com/springboot2essentials/model/Anime.java
@@ -1,13 +1,20 @@
 package br.com.springboot2essentials.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 
 @Data
 @AllArgsConstructor
+@Entity
 public class Anime {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
     private String name;
     
--- src/main/java/br/com/springboot2essentials/repository/AnimeRepository.java
+++ src/main/java/br/com/springboot2essentials/repository/AnimeRepository.java
@@ -2,12 +2,12 @@ package br.com.springboot2essentials.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.springboot2essentials.model.Anime;
 
 @Repository
-public interface AnimeRepository {
+public interface AnimeRepository extends JpaRepository<Anime, Long>{
 	
-	public List<Anime> listAll();
 }
